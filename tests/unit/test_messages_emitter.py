35c461f4c3d87714e39dfad34e30d2aa4b4d6839
Facundo Batista <facundo@taniquetil.com.ar>
2021-08-25 13:13:01 -0300

    Introducing Emitter and _Printer (minimum usable code to bootsrap the project)

---
+++ tests/unit/test_messages_emitter.py
@@ -0,0 +1,191 @@
+#
+# Copyright 2021 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""Tests that check the whole Emitter machinery."""
+
+import sys
+from unittest.mock import patch, call
+
+import pytest
+
+from craft_cli.messages import Emitter, EmitterMode
+
+
+@pytest.fixture
+def get_initiated_emitter():
+    """Provide an initiated Emitter ready to test.
+
+    It has a patched "printer" and an easy way to test its calls (after it was initiated).
+
+    It's used almost in all tests (except those that test the init call).
+    """
+    with patch("craft_cli.messages._Printer", autospec=True) as mock_printer:
+
+        def f(mode, greeting="default greeting"):
+            e = Emitter()
+            e.init(mode, greeting)
+            e.printer_calls = mock_printer.mock_calls
+            e.printer_calls.clear()
+            return e
+
+        yield f
+
+
+# -- tests for init and setting mode
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        EmitterMode.QUIET,
+        EmitterMode.NORMAL,
+    ],
+)
+def test_init_quietish(mode):
+    """Init the class in some quiet-ish mode."""
+    greeting = "greeting"
+    emitter = Emitter()
+    with patch("craft_cli.messages._Printer") as mock_printer:
+        emitter.init(mode, greeting)
+
+    assert emitter.mode == mode
+    assert mock_printer.mock_calls == [
+        call(),  # the _Printer instantiation
+    ]
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        EmitterMode.VERBOSE,
+        EmitterMode.TRACE,
+    ],
+)
+def test_init_verboseish(mode):
+    """Init the class in some verbose-ish mode."""
+    greeting = "greeting"
+    emitter = Emitter()
+    with patch("craft_cli.messages._Printer") as mock_printer:
+        emitter.init(mode, greeting)
+
+    assert emitter.mode == mode
+    assert mock_printer.mock_calls == [
+        call(),  # the _Printer instantiation
+        call().show(sys.stderr, greeting, use_timestamp=True, end_line=True),
+    ]
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        EmitterMode.QUIET,
+        EmitterMode.NORMAL,
+    ],
+)
+def test_set_mode_quietish(get_initiated_emitter, mode):
+    """Set the class to some quiet-ish mode."""
+    greeting = "greeting"
+    emitter = get_initiated_emitter(EmitterMode.QUIET, greeting=greeting)
+    emitter.set_mode(mode)
+
+    assert emitter.mode == mode
+    assert emitter.printer_calls == []
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        EmitterMode.VERBOSE,
+        EmitterMode.TRACE,
+    ],
+)
+def test_set_mode_verboseish(get_initiated_emitter, mode):
+    """Set the class to some verbose-ish mode."""
+    greeting = "greeting"
+    emitter = get_initiated_emitter(EmitterMode.QUIET, greeting=greeting)
+    emitter.set_mode(mode)
+
+    assert emitter.mode == mode
+    assert emitter.printer_calls == [
+        call().show(sys.stderr, greeting, use_timestamp=True, end_line=True),
+    ]
+
+
+# -- tests for emitting messages of all kind
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        EmitterMode.QUIET,
+        EmitterMode.NORMAL,
+        EmitterMode.VERBOSE,
+        EmitterMode.TRACE,
+    ],
+)
+def test_message_final(get_initiated_emitter, mode):
+    """Emit a final message."""
+    emitter = get_initiated_emitter(mode)
+    emitter.message("some text")
+
+    assert emitter.printer_calls == [
+        call().show(sys.stdout, "some text", use_timestamp=False),
+    ]
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        EmitterMode.QUIET,
+        EmitterMode.NORMAL,
+    ],
+)
+def test_message_intermediate_quietish(get_initiated_emitter, mode):
+    """Emit an intermediate message when in a quiet-ish mode."""
+    emitter = get_initiated_emitter(mode)
+    emitter.message("some text", intermediate=True)
+
+    assert emitter.printer_calls == [
+        call().show(sys.stdout, "some text", use_timestamp=False),
+    ]
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        EmitterMode.VERBOSE,
+        EmitterMode.TRACE,
+    ],
+)
+def test_message_intermediate_verboseish(get_initiated_emitter, mode):
+    """Emit an intermediate message when in a verbose-ish mode."""
+    emitter = get_initiated_emitter(mode)
+    emitter.message("some text", intermediate=True)
+
+    assert emitter.printer_calls == [
+        call().show(sys.stdout, "some text", use_timestamp=True),
+    ]
+
+
+# -- tests for stopping the machinery
+
+
+def test_ended_ok(get_initiated_emitter):
+    """Finish everything."""
+    emitter = get_initiated_emitter(EmitterMode.QUIET)
+    emitter.ended_ok()
+
+    assert emitter.printer_calls == [call().stop()]
---
+++ tests/unit/test_messages_integration.py
@@ -0,0 +1,216 @@
+#
+# Copyright 2021 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""Tests that check the whole message machinery.
+
+These are "integration" tests in the sense that call the external API and verify final
+outputs.
+
+Most of the different cases here mimic the specification table in:
+
+    https://docs.google.com/document/d/1Pe-0ED6db53SmrUGIAgVMzxeCOGJQwsZJWf7jzFSagQ/
+"""
+
+import logging
+import re
+from dataclasses import dataclass
+
+import pytest
+
+from craft_cli import messages
+from craft_cli.messages import Emitter, EmitterMode
+
+
+# the timestamp format (including final separator space)
+TIMESTAMP_FORMAT = r"\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d.\d\d\d "
+
+# the greeting sent, normalized across the tests so we can automatically ignore it
+GREETING = "Specific greeting to be ignored"
+
+
+@pytest.fixture
+def logger():
+    """Provide a logger with an empty set of handlers."""
+    logger = logging.getLogger()
+    logger.setLevel(0)
+    logger.handlers.clear()
+    return logger
+
+
+@dataclass
+class Line:
+    """A line that is expected to be in the result."""
+
+    text: str
+    permanent: bool = True  # if it should be overwritten by next message or not
+    timestamp: bool = False  # if it should be prefixed by a timestamp
+
+
+def compare_lines(expected_lines, raw_stream):
+    """Helper to compare expected lines to what was written to the terminal."""
+    width = messages.get_terminal_width()
+    if expected_lines:
+        assert len(raw_stream) > 0
+    assert len(raw_stream) % width == 0, f"Bad length {len(raw_stream)} ({width=}) {raw_stream=!r}"
+    args = [iter(raw_stream)] * width
+    lines = ["".join(x) for x in zip(*args)]
+    if lines and GREETING in lines[0]:
+        lines = lines[1:]
+
+    assert len(expected_lines) == len(lines)
+    for expected, real in zip(expected_lines, lines):
+        end_of_line = "\n" if expected.permanent else "\r"
+        timestamp = TIMESTAMP_FORMAT if expected.timestamp else ""
+
+        # the timestamp (if should be there), the text to compare, some spaces, and the CR/LN
+        template = f"{timestamp}(.*?) *{end_of_line}"
+        m = re.match(template, real)
+        assert m, f"Line {real!r} didn't match {template!r}"
+        assert m.groups()[0] == expected.text
+
+
+def assert_outputs(capsys, emit, expected_out=None, expected_err=None):
+    """Verify that the outputs are correct according to the expected lines."""
+    # check the expected stdout and stderr outputs
+    out, err = capsys.readouterr()
+    if expected_out is None:
+        assert not out
+    else:
+        compare_lines(expected_out, out)
+    if expected_err is None:
+        compare_lines([], err)  # this comparison will eliminate the greeting
+    else:
+        compare_lines(expected_err, err)
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        EmitterMode.QUIET,
+        EmitterMode.NORMAL,
+        EmitterMode.VERBOSE,
+        EmitterMode.TRACE,
+    ],
+)
+def test_01_expected_cmd_result(capsys, mode):
+    """Show a simple message, the expected command result."""
+    emit = Emitter()
+    emit.init(mode, GREETING)
+    emit.message("The meaning of life is 42.")
+    emit.ended_ok()
+
+    expected = [
+        Line("The meaning of life is 42."),
+    ]
+    assert_outputs(capsys, emit, expected_out=expected)
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        EmitterMode.QUIET,
+        EmitterMode.NORMAL,
+    ],
+)
+def test_01_intermediate_message_quiet(capsys, mode):
+    """Show an intermediate message, in more quiet modes."""
+    emit = Emitter()
+    emit.init(mode, GREETING)
+    emit.message("The meaning of life is 42.", intermediate=True)
+    emit.ended_ok()
+
+    expected = [
+        Line("The meaning of life is 42."),
+    ]
+    assert_outputs(capsys, emit, expected_out=expected)
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        EmitterMode.VERBOSE,
+        EmitterMode.TRACE,
+    ],
+)
+def test_01_intermediate_message_verbose(capsys, mode):
+    """Show an intermediate message, in more verbose modes."""
+    emit = Emitter()
+    emit.init(mode, GREETING)
+    emit.message("The meaning of life is 42.", intermediate=True)
+    emit.ended_ok()
+
+    expected = [
+        Line("The meaning of life is 42.", timestamp=True),
+    ]
+    assert_outputs(capsys, emit, expected_out=expected)
+
+
+@pytest.mark.parametrize(
+    "mode",
+    [
+        EmitterMode.QUIET,
+        EmitterMode.NORMAL,
+    ],
+)
+def test_greeting_when_quiet(capsys, mode):
+    """Check when the greeting is sent when setting the mode to more quiet modes."""
+    different_greeting = "different greeting to not be ignored"
+    emit = Emitter()
+    emit.init(EmitterMode.NORMAL, different_greeting)
+    emit.set_mode(mode)
+    emit.message("final message")
+    emit.ended_ok()
+
+    expected_out = [
+        Line("final message"),
+    ]
+    assert_outputs(capsys, emit, expected_out=expected_out)
+
+
+def test_greeting_when_verbose(capsys):
+    """Check when the greeting is sent when setting the mode to VERBOSE."""
+    different_greeting = "different greeting to not be ignored"
+    emit = Emitter()
+    emit.init(EmitterMode.NORMAL, different_greeting)
+    emit.set_mode(EmitterMode.VERBOSE)
+    emit.message("final message")
+    emit.ended_ok()
+
+    expected_out = [
+        Line("final message"),
+    ]
+    expected_err = [
+        Line(different_greeting, timestamp=True),
+    ]
+    assert_outputs(capsys, emit, expected_out=expected_out, expected_err=expected_err)
+
+
+def test_greeting_when_trace(capsys):
+    """Check when the greeting is sent when setting the mode to TRACE."""
+    different_greeting = "different greeting to not be ignored"
+    emit = Emitter()
+    emit.init(EmitterMode.NORMAL, different_greeting)
+    emit.set_mode(EmitterMode.TRACE)
+    emit.message("final message")
+    emit.ended_ok()
+
+    expected_out = [
+        Line("final message"),
+    ]
+    expected_err = [
+        Line(different_greeting, timestamp=True),
+    ]
+    assert_outputs(capsys, emit, expected_out=expected_out, expected_err=expected_err)
