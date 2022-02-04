f1a95a2ce7ccaf34c4ce3579ac6177b0fc66d065
Facundo Batista <facundo@taniquetil.com.ar>
2022-02-04 08:48:35 -0300

    Restore old examples functionality.

--- examples.py
+++ examples.py
@@ -226,5 +226,19 @@ if func is None:
     exit()
 
 emit.init(EmitterMode.NORMAL, "explorator", "Greetings earthlings")
-value = 42
-emit.message(f"The meaning of life is {value}.")
+try:
+    func()
+except CraftError as err:
+    emit.error(err)
+except KeyboardInterrupt as exc:
+    msg = "User cancelled"
+    error = CraftError(msg)
+    error.__cause__ = exc
+    emit.error(error)
+except Exception as exc:
+    msg = f"Unexpected internal exception: {exc!r}"
+    error = CraftError(msg)
+    error.__cause__ = exc
+    emit.error(error)
+else:
+    emit.ended_ok()
