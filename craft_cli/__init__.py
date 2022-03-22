f927c65530e84aeb09ebbc67c8043dd875f398e5
Facundo Batista <facundo@taniquetil.com.ar>
2022-03-22 11:52:46 -0300

    Release 0.3.1

--- craft_cli/__init__.py
+++ craft_cli/__init__.py
@@ -16,7 +16,7 @@
 
 """Interact with Canonical services such as Charmhub and the Snap Store."""
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 # names included here only to be exposed as external API; the particular order of imports
 # is to break cyclic dependencies
