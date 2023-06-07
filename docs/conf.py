802f04301cccd8464e2577ea3addc38610a3c368
Tiago Nobrega <tiago.nobrega@canonical.com>
2023-06-07 16:48:40 -0300

    docs: consolidate craft-cli and starbase docs
    This commit brings craft-cli's documentation into the format and build
    process that Starbase imposes. The specific Makefile and requirements
    files in docs/ are removed in favor of the tox 'build-docs' and
    'autobuild-docs' that use the dependencies from pyproject.toml.
    
    sphinx-build is called *without* treating warnings as errors, because
    the auto-generated doc process generates a bunch of warnings about
    circular imports and duplicate declarations that should be fixed at
    a later date.
    
    Also remove .readthedocs.yml in favor of the starbase-provided
    .readthedocs.yaml.

--- docs/conf.py
+++ docs/conf.py
@@ -2,13 +2,22 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath(".."))
+
+import craft_cli  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "starcraft"
+project = "Craft CLI"
 copyright = "2023, Canonical"
 author = "Canonical"
+
+release = craft_cli.__version__
 
 # region General configuration
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -54,9 +63,36 @@ intersphinx_mapping = {
 set_type_checking_flag = True
 typehints_fully_qualified = False
 always_document_param_types = True
+typehints_document_rtype = True
 
 # Github config
 github_username = "canonical"
-github_repository = "starcraft-base"
+github_repository = "craft-cli"
+# endregion
+
+
+# region Setup reference generation
+def run_apidoc(_):
+    from sphinx.ext.apidoc import main
+    import os
+    import sys
+
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    module = os.path.join(cur_dir, "..", "craft_cli")
+    exclude_patterns = ["*pytest_plugin*"]
+    main(["-e", "--no-toc", "--force", "-o", cur_dir, module, *exclude_patterns])
+
+
+def no_namedtuple_attrib_docstring(app, what, name, obj, options, lines):
+    """Strips out silly "Alias for field number" lines in namedtuples reference."""
+    if len(lines) == 1 and lines[0].startswith("Alias for field number"):
+        del lines[:]
+
+
+def setup(app):
+    app.connect("builder-inited", run_apidoc)
+    app.connect("autodoc-process-docstring", no_namedtuple_attrib_docstring)
+
 
 # endregion
--- docs/index.rst
+++ docs/index.rst
@@ -1,24 +1,22 @@
-.. starcraft documentation root file
-
-StarCraft
-=========
+Welcome to Craft CLI's documentation!
+=====================================
 
 .. toctree::
-   :maxdepth: 1
+   :maxdepth: 2
    :hidden:
 
-   tutorials/index
-   howto/index
-   reference/index
-   explanation/index
+   tutorials
+   howtos
+   reference
+   explanations
 
 .. grid:: 1 1 2 2
 
-   .. grid-item-card:: :ref:`Tutorial <tutorial>`
+   .. grid-item-card:: :ref:`Tutorial <tutorials>`
 
-      **Get started** with a hands-on introduction to Starcraft
+      **Get started** with hands-on introductions to Craft CLI
 
-   .. grid-item-card:: :ref:`How-to guides <howto>`
+   .. grid-item-card:: :ref:`How-to guides <howtos>`
 
       **Step-by-step guides** covering key operations and common tasks
 
@@ -27,22 +25,12 @@ StarCraft
 
    .. grid-item-card:: :ref:`Reference <reference>`
 
-      **Technical information** about Starcraft
+      **Technical information** about Craft CLI
 
-   .. grid-item-card:: :ref:`Explanation <explanation>`
+   .. grid-item-card:: :ref:`Explanation <explanations>`
 
       **Discussion and clarification** of key topics
 
-Project and community
-=====================
-
-Starcraft is a member of the Canonical family. It's an open source project
-that warmly welcomes community projects, contributions, suggestions, fixes
-and constructive feedback.
-
-* `Ubuntu Code of Conduct <https://ubuntu.com/community/code-of-conduct>`_.
-* `Canonical contributor licenses agreement
-  <https://ubuntu.com/legal/contributors>`_.
 
 Indices and tables
 ==================
